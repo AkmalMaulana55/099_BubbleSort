310b5f79b4095240dbee580d675ab3a1f6567008
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-03-13 19:39:47 +0700

    first commit

---
+++ BubbleSort.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int arr[10]; // variable public stray
+int nData;
+
+void inputData()
+{
+    while (true)
+    {
+        cout << "Masukkan jumlah data= ";
+        cin >> nData;
+
+        if (nData < 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Jumlah data maksimal 10" << endl; 
+        }
+    }
+    
+    cout << "================" << endl;
+    cout << "Masukkan Data" << endl;
+    cout << "================" << endl;
+    cout << endl;
+
+    for (int i = 0; i < nData; i++)
+    {
+        cout << "input data ke-" << i + 1 << "=";
+        cin >> arr[i];
+    }
+    
+}
+
+void bubleSort(){
+    int  pass = 1; // step 1
+
+    do
+    {
+        for(int j = 0; j <= nData - 1 - pass; j++) // step 2
+        {
+            if (arr[j] > arr[j + 1]) // step 3
+            {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    
+        pass = pass + 1;  // step 4
+    } while (pass <= nData - 1);
+}
+
+void tampilData(){
+    cout << "Tampil Data" << endl;
+    cout << endl;
+
+    for (int i = 0; i < nData; i++)
+    {
+        cout << "Data ke - " << i + 1 << " = " << arr[1] << endl;
+    }
+    
+}
+
+int main() {
+    inputData();
+    bubleSort();
+    tampilData();
+    return 0;
+}
